1a3d24f4774c3d7c3d101f515e0c70a065124d56
Norman Fomferra <norman.fomferra@gmail.com>
2019-02-19 15:54:06 +0100

    Internationlized UI

--- src/states/configState.ts
+++ src/states/configState.ts
@@ -1,4 +1,4 @@
-import { DEFAULT_APP_NAME, DEFAULT_API_SERVER_URL } from "../api/config";
+import { DEFAULT_APP_NAME, DEFAULT_API_SERVER_URL } from "../config/config";
 
 export interface ConfigState {
     appName: string;
