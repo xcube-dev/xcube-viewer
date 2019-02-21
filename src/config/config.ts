45b4dce027e9ed7ade9e948a5115dc9f045ff19f
Norman Fomferra <norman.fomferra@gmail.com>
2019-02-21 12:49:52 +0100

    added swedisch transl.

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,32 +5,41 @@ export const DEFAULT_API_SERVER_URL = 'http://localhost:8080/xcube/api/0.1.0.dev
 
 export const I18N = new Database();
 
-I18N.locale = "de";
+I18N.locale = "se";
 
 I18N.set('en')`Dataset`
-    .add('de')`Datensatz`;
+    .add('de')`Datensatz`
+    .add('se')`Uppgifter`;
 
 I18N.set('en')`Variable`
-    .add('de')`Variable`;
+    .add('de')`Variable`
+    .add('se')`Variabel`;
 
 I18N.set('en')`Places`
-    .add('de')`Orte`;
+    .add('de')`Orte`
+    .add('se')`Platser`;
 
 I18N.set('en')`Place`
-    .add('de')`Ort`;
+    .add('de')`Ort`
+    .add('se')`Plats`;
 
 I18N.set('en')`Time`
-    .add('de')`Zeit`;
+    .add('de')`Zeit`
+    .add('se')`Tid`;
 
 I18N.set('en')`Multi`
-    .add('de')`Multi`;
+    .add('de')`Multi`
+    .add('se')`Multi`;
 
 I18N.set('en')`Something went wrong.`
-    .add('de')`Irgendetwas lief schief.`;
+    .add('de')`Irgendetwas lief schief.`
+    .add('se')`Något gick fel.`;
 
 I18N.set('en')`Time-Series`
-    .add('de')`Zeitserie`;
+    .add('de')`Zeitserie`
+    .add('se')`Tidsserier`;
 
 I18N.set('en')`Values`
-    .add('de')`Werte`;
+    .add('de')`Werte`
+    .add('se')`Värden`;
 
