bb19119b0a49c01c993ca98f3275e15ef21efc40
Norman Fomferra <norman.fomferra@gmail.com>
2019-02-22 10:51:18 +0100

    adapted to new API

--- src/api/getDatasets.ts
+++ src/api/getDatasets.ts
@@ -2,6 +2,6 @@ import { callJsonApi } from './callApi';
 import { Dataset } from '../model/dataset';
 
 export function getDatasets(apiServerUrl: string): Promise<Dataset[]> {
-    return callJsonApi<Dataset[]>(apiServerUrl + '/datasets?deep=1&tiles=ol4')
+    return callJsonApi<Dataset[]>(apiServerUrl + '/datasets?details=1&tiles=ol4')
         .then(result => result['datasets']);
 }
