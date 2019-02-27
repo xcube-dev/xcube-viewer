071aaed8ba6b61dd85c87fc85f47d1592ff6a2e7
Helge <helge.dzierzon@brockmann-consult.de>
2019-02-27 11:37:13 +0100

    - Simplified the time chart strole color config. Uses now teh HUE[COLOR] syntax.

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,14 @@ export const VIEWER_SECONDARY_COLOR = pink;
 export const VIEWER_LOGO_WIDTH = 32;
 export const VIEWER_HEADER_BACKGROUND_COLOR = undefined;
 
-export const LINECHART_STROKES = [grey.A100, red.A100, blue.A100, green.A100, yellow.A100];
+export const LINECHART_STROKE_SHADE = 200;
+export const LINECHART_STROKES = [
+    grey[LINECHART_STROKE_SHADE],
+    red[LINECHART_STROKE_SHADE],
+    blue[LINECHART_STROKE_SHADE],
+    green[LINECHART_STROKE_SHADE],
+    yellow[LINECHART_STROKE_SHADE]
+];
 
 
 export const I18N = new Database();
