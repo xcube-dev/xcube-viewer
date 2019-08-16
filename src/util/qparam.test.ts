cd578fa23cf2acaf54b5429b3910467afb8a29a2
Norman Fomferra <norman.fomferra@gmail.com>
2019-08-16 13:36:25 +0200

    more tests

--- src/util/qparam.test.ts
+++ src/util/qparam.test.ts
@@ -1,4 +1,3 @@
-
 import { getQueryParameterByName } from './qparam';
 
 
@@ -15,6 +14,23 @@ describe('getQueryParameterByName', () => {
 
         const actualServerName = getQueryParameterByName(queryStr, 'serverName');
         expect(actualServerName).toEqual(serverName);
+    });
+
+    it('works with default (missing param)', () => {
+        const serverUrl = 'http://xcube-org:3000/xcube-server/api/v1.0';
+        const serverName = 'Default Server';
+
+        const queryStr = `?server=${encodeURIComponent(serverUrl)}&serverName=${encodeURIComponent(serverName)}`;
+
+        const actualServerUrl = getQueryParameterByName(queryStr, 'serverUrl', 'https://pippo');
+        expect(actualServerUrl).toEqual('https://pippo');
+        const actualServerName = getQueryParameterByName(queryStr, 'serverName', 'https://pippo');
+        expect(actualServerName).toEqual(serverName);
+    });
+
+    it('works with default (no queryStr)', () => {
+        const actualServerUrl = getQueryParameterByName(null, 'serverUrl', 'https://pippo');
+        expect(actualServerUrl).toEqual('https://pippo');
     });
 
 });
