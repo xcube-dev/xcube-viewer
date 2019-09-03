284085f82b35a52a64cb0af8ff0326fb6f04f7e1
Norman Fomferra <norman.fomferra@gmail.com>
2019-09-03 21:04:30 +0200

    closes #81

--- src/components/ol/Map.tsx
+++ src/components/ol/Map.tsx
@@ -30,11 +30,23 @@ const DEFAULT_CONTAINER_SYTLE: React.CSSProperties = {height: '100%'};
 
 export class Map extends React.Component<MapProps, MapState> {
 
-    private contextValue: MapContext;
+    private readonly contextValue: MapContext;
 
     constructor(props: MapProps) {
         super(props);
-        this.contextValue = {mapObjects: this.props.mapObjects || {}};
+        console.log("Map.constructor");
+
+        const {id, mapObjects} = props;
+        if (mapObjects) {
+            this.contextValue = {
+                map: mapObjects[id] as ol.Map || undefined,
+                mapObjects: mapObjects
+            };
+        } else {
+            this.contextValue = {
+                mapObjects: {}
+            };
+        }
     }
 
     private getMapOptions(): ol.olx.MapOptions {
@@ -80,36 +92,54 @@ export class Map extends React.Component<MapProps, MapState> {
     };
 
     componentDidMount(): void {
+        console.log("Map.componentDidMount");
+        const {id, mapObjects} = this.props;
         const target = this.contextValue.mapDiv!;
-        const initialZoom = this.getMinZoom(target);
-        // console.log("Map.componentDidMount: new Map!");
-        const view = new ol.View({
-                                     center: ol.proj.fromLonLat([0, 0]),
-                                     minZoom: initialZoom,
-                                     zoom: initialZoom,
-                                 });
-        const map = new ol.Map({
-                                   view,
-                                   ...this.getMapOptions(),
-                                   target
-                               });
-        map.set("objectId", this.props.id);
-        this.contextValue.map = map;
-        this.contextValue.mapObjects[this.props.id] = map;
 
+        let map: ol.Map | undefined;
+        if (mapObjects && mapObjects[id]) {
+            // TODO (forman): perform more tests to verify it is a ol.Map object
+            map = mapObjects[id] as ol.Map;
+        }
+
+        console.log("Map.componentDidMount: ", map, mapObjects);
+
+        if (!map) {
+            const initialZoom = this.getMinZoom(target);
+            const view = new ol.View({
+                                         center: ol.proj.fromLonLat([0, 0]),
+                                         minZoom: initialZoom,
+                                         zoom: initialZoom,
+                                     });
+            map = new ol.Map({
+                                 view,
+                                 ...this.getMapOptions(),
+                                 target
+                             });
+        }
+
+        this.contextValue.map = map;
+        if (mapObjects) {
+            mapObjects[id] = map;
+            this.contextValue.mapObjects = mapObjects;
+        } else {
+            this.contextValue.mapObjects = {[id]: map};
+        }
+
+        map.set("objectId", this.props.id);
         map.on('click', this.handleClick);
 
         // Force update so we can pass this.map as context to all children in next render()
         this.forceUpdate();
 
+        // Add resize listener so we can adjust the view's minZoom.
+        // See https://openlayers.org/en/latest/examples/min-zoom.html
+        window.addEventListener('resize', this.handleResize);
+
         const onMapRef = this.props.onMapRef;
         if (onMapRef) {
             onMapRef(map);
         }
-
-        // Add resize listener so we can adjust the view's minZoom.
-        // See https://openlayers.org/en/latest/examples/min-zoom.html
-        window.addEventListener('resize', this.handleResize);
     }
 
     componentDidUpdate(prevProps: Readonly<MapProps>): void {
@@ -120,12 +150,17 @@ export class Map extends React.Component<MapProps, MapState> {
     }
 
     componentWillUnmount(): void {
-        // console.log("Map.componentWillUnmount: remove Map!");
-        const onMapRef = this.props.onMapRef;
-        if (onMapRef) {
-            onMapRef(null);
+        const {mapObjects} = this.props;
+        console.log("Map.componentWillUnmount: ", mapObjects);
+        if (!mapObjects) {
+            const onMapRef = this.props.onMapRef;
+            if (onMapRef) {
+                onMapRef(null);
+            }
         }
-        this.contextValue = {mapObjects: {}};
+
+        // Remove resize listener so we can adjust the view's minZoom.
+        window.removeEventListener('resize', this.handleResize);
     }
 
     render() {
