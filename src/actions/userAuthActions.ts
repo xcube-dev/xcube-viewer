f6bd440ebe94862dd39fcd53f90cb65b958b1547
Norman Fomferra <norman.fomferra@gmail.com>
2020-02-12 17:14:53 +0100

    Reload datasets after users are authenticated

--- src/actions/userAuthActions.ts
+++ src/actions/userAuthActions.ts
@@ -24,6 +24,7 @@ export function initAuthClient() {
             }
             if (idToken && accessToken) {
                 dispatch(receiveSignIn(idToken!, accessToken!));
+                dispatch(updateDatasets() as any);
             }
         }
     };
