20d8d17ec274fc24fac8ca3cb29076bbbf89d57c
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2024-05-22 10:30:36 +0200

    Renamed WTS into XYZ

--- src/util/maps.ts
+++ src/util/maps.ts
@@ -22,8 +22,9 @@
  * SOFTWARE.
  */
 
-// Thanks to alexurquhart, maps.json is content of https://github.com/alexurquhart/free-tiles/blob/master/tiles.json
-// Check http://alexurquhart.github.io/free-tiles/
+// Thanks to alexurquhart, maps.json is content of
+// https://github.com/alexurquhart/free-tiles/blob/master/tiles.json.
+// Check http://alexurquhart.github.io/free-tiles/.
 import _maps from "@/resources/maps.json";
 
 export const maps = _maps as MapGroup[];
