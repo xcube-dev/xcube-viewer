1d584dc606561a47a802ad950280c93c0918aaad
ruchimotwaniBC <ruchi.motwani@brockmann-consult.de>
2024-09-11 16:36:33 +0200

    changes

--- src/components/ol/ExportMap.tsx
+++ src/components/ol/ExportMap.tsx
@@ -2,8 +2,7 @@ import { Map as OlMap } from "ol";
 
 export const exportMapAsImage = (
   map: OlMap,
-  //format: string = "a4",
-  resolution: number = 2,
+  resolution: number = 4
 ) => {
   const mapElement = map.getTargetElement();
   if (!mapElement) {
@@ -14,32 +13,52 @@ export const exportMapAsImage = (
 
   const mapCanvas = document.createElement("canvas");
   const size = map.getSize();
+
   if (size) {
-    mapCanvas.width = 800 * resolution;
-    mapCanvas.height = 800 * resolution;
+    const width = size[0];
+    const height = size[1];
+
+
+    mapCanvas.width = width * resolution;
+    mapCanvas.height = height * resolution;
   }
+
   const mapContext = mapCanvas.getContext("2d");
 
   if (mapContext) {
+
+    mapContext.scale(resolution, resolution);
+
+
     Array.prototype.forEach.call(
-      document.querySelectorAll(".ol-layer canvas"),
+      document.querySelectorAll(".ol-viewport canvas, .ol-unselectable .ol-control"),
       (canvas: HTMLCanvasElement) => {
         if (canvas.width > 0) {
           const opacity = canvas.style.opacity || 1;
           mapContext.globalAlpha = Number(opacity);
+          let matrix: number[];
           const transform = canvas.style.transform;
-          const matrix = transform
-            .match(/^matrix\(([^\(]*)\)$/)?.[1]
-            .split(",")
-            .map(Number) || [1, 0, 0, 1, 0, 0];
-          mapContext.setTransform(
-            matrix[0],
-            matrix[1],
-            matrix[2],
-            matrix[3],
-            matrix[4],
-            matrix[5],
-          );
+          if (transform) {
+            matrix = transform
+              .match(/^matrix\(([^\(]*)\)$/)?.[1]
+              .split(',')
+              .map(Number) || [1, 0, 0, 1, 0, 0];
+          } else {
+            matrix = [
+              parseFloat(canvas.style.width) / canvas.width || 1,
+              0,
+              0,
+              parseFloat(canvas.style.height) / canvas.height || 1,
+              0,
+              0,
+            ];
+          }
+
+          if (matrix.length === 6) {
+            mapContext.setTransform(
+              matrix[0], matrix[1], matrix[2], matrix[3], matrix[4], matrix[5]
+            );
+          }
           mapContext.drawImage(canvas, 0, 0);
         }
       },
@@ -56,15 +75,20 @@ export const exportMapAsImage = (
 
     image.onload = () => {
       mapContext.clearRect(0, 0, mapCanvas.width, mapCanvas.height);
-      mapContext.drawImage(image, mapCanvas.width / 2 - 40, 25, 80, 120);
+
+      mapContext.drawImage(
+        image,
+        0, 0,
+        mapCanvas.width,
+        mapCanvas.height
+      );
 
       mapCanvas.toBlob((blob) => {
         if (blob) {
           const item = new ClipboardItem({ "image/png": blob });
           navigator.clipboard.write([item]).then(
             () => {
-              //TODO- Add this to dispatch and remove below code
-              alert("Map image copied to clipboard.");
+              alert("map image copied to clipboard.");
             },
             (error) => {
               console.error("Error copying image to clipboard: ", error);
@@ -74,7 +98,6 @@ export const exportMapAsImage = (
         }
       }, "image/png");
 
-      map.setSize(size);
       document.body.style.cursor = "auto";
     };
   }
