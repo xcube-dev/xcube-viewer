6fda14cdf40b65026076497b82695342cb58dc06
Yogesh Kumar Baljeet Singh <yogesh09121995@gmail.com>
2024-10-16 17:52:06 +0200

    add else case

--- src/store/registerSlices.ts
+++ src/store/registerSlices.ts
@@ -9,12 +9,21 @@ export interface SliceType {
 export type Slice = (Partial<ControlState> | Partial<DataState>) & SliceType;
 
 export const registerSlice = (slice: Slice) => {
+  const { type: _type, ...remainingSlice } = slice;
   if (slice.type == "control") {
     store.setState((state) => ({
       ...state,
       ["controlState"]: {
         ...state.controlState,
-        ...slice,
+        ...remainingSlice,
+      },
+    }));
+  } else if (slice.type == "data") {
+    store.setState((state) => ({
+      ...state,
+      ["dataState"]: {
+        ...state.dataState,
+        ...remainingSlice,
       },
     }));
   }
