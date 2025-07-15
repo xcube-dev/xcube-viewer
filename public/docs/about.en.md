e1e2a4396225033ee6d61e1bde3d804492755b48
clarasb <clara.backens@web.de>
2025-07-15 16:48:35 +0200

    update components/Markdown.tsx

--- public/docs/about.en.md
+++ public/docs/about.en.md
@@ -1,6 +1,5 @@
 [![xcube Viewer](/images/logo_xcube.png)](https://xcube-dev.github.io/xcube-viewer/)
 
-
 Welcome to **xcube Viewer**, which is a single-page
 web application that provides tools to visualise and analyse multitemporal 
 spatial datasets. The data is provided via [xcube Server](https://xcube.readthedocs.io/en/latest/webapi.html).
--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -47,7 +47,7 @@ export default function Markdown({ text }: MarkdownProps) {
         const { node: _, ...rest } = props;
         return (
           <img
-            style={{ maxWidth: "100%", display: "block", margin: "0 auto" }}
+            style={{ maxWidth: "100%", display: "block", margin: "1rem auto" }}
             {...rest}
           />
         );
