eff2a4b53e76cfd70d80fcf0b5caa46bdd858519
clarasb <clara.backens@web.de>
2025-10-13 17:40:13 +0200

    add setDatasetZLevel to hook dependency

--- src/components/Viewer/Viewer.tsx
+++ src/components/Viewer/Viewer.tsx
@@ -390,7 +390,7 @@ export default function Viewer({
         setDatasetZLevel(datasetZLevel);
       }
     }
-  }, [map, variableLayer]);
+  }, [map, variableLayer, setDatasetZLevel]);
 
   return (
     <ErrorBoundary>
