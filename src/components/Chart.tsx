d840453d8aac34ba220f65982a07d43189cf3036
vanta.nox <200978133+Ap-0007@users.noreply.github.com>
2026-07-13 21:40:06 +0530

    fix: tooltip width

---
+++ src/components/Chart.tsx
@@ -0,0 +1,9 @@
+const tooltipStyles = {
+  position: 'absolute',
+  backgroundColor: 'white',
+  border: '1px solid black',
+  padding: '10px',
+  maxWidth: '80%',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis'
+};
