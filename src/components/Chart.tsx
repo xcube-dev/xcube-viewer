f70ce647c65efd3a9b3779ec33853e15491259f0
vanta.nox <200978133+Ap-0007@users.noreply.github.com>
2026-07-13 21:40:43 +0530

    fix: chart layout stability

---
+++ src/components/Chart.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const Chart = () => {
+  return (
+    <Box sx={{ maxHeight: '100vh', overflowY: 'auto' }}>
+      {/* chart content */}
+    </Box>
+  );
+};
